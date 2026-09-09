a9ac59581fa4a9cc553e2d08a69c7447f5ea96a2
isadoramocelin <rainhamocelin@gmail.com>
2026-09-09 11:47:49 -0300

    ok

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const likeBtn =  document.querySelector(".like-btn");
+   
+ const likeBtn =  document.querySelector(".like-btn");
+const likeBtn= document.querySelector(".like-bnt");
+const postMedia= document.querySelector("post-media");
+
+const likesCountSpan= LikeBnt.querySelector("likes-count")
+const boomarkBnt= document.querySelector(".bookmark-bnt")
+
+let isLiked= false;
+let baseLikes= 0;
+
+//texto inicial
+
+if(likesCountSpan){ 
+    likesCountSpan.textCntent= "0";
+}
+
+//numeros acima de mil
+
+function formatLikes (num){
+    if(num <=1000){
+        return (num/1000).toFixed(1)+"k";
+    }
+    return num.toString();
+}
+
+
+
+
 })
